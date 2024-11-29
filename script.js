a2884707ae48cfe86ccd6880a17605d73f3cb067
PikriaP <ffilishvili@gmail.com>
2024-11-30 03:16:39 +0400

    version 2

--- script.js
+++ script.js
@@ -151,3 +151,14 @@
 //     return str.split('').reverse().join('');
 // }
 // console.log(reverseString("happy holiday"));
+// version 2.
+function reverseString(str){
+    let reversed ="";
+    for(let i= str.length-1; i>=0; i--){
+        reversed+=str[i];
+    }
+    return reversed;
+}
+const string = 'happy holiday';
+const reversedString=reverseString(string);
+console.log(reversedString);
