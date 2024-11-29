f9063bab0c30b6ed380428c79487c3b73cba6601
PikriaP <ffilishvili@gmail.com>
2024-11-29 23:47:24 +0400

    ex. 9
    ex. 9

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
-const inputElement = document.getElementById('value1');
-const staticValueElement = document.getElementById('value2');
-const total = document.getElementById('total');
+// const inputElement = document.getElementById('value1');
+// const staticValueElement = document.getElementById('value2');
+// const total = document.getElementById('total');
 
-inputElement.addEventListener('input', () => {
-    const inputValue = parseFloat(inputElement.value);
-    const result = inputValue * parseInt(staticValueElement.textContent);
+// inputElement.addEventListener('input', () => {
+//     const inputValue = parseFloat(inputElement.value);
+//     const result = inputValue * parseInt(staticValueElement.textContent);
 
-    if (result > 0) {
-        total.classList.remove('other');
-        total.classList.add('positive');
-    } else {
-        total.classList.remove('positive');
-        total.classList.add('other');
-    }
+//     if (result > 0) {
+//         total.classList.remove('other');
+//         total.classList.add('positive');
+//     } else {
+//         total.classList.remove('positive');
+//         total.classList.add('other');
+//     }
 
-    total.textContent = result;
+//     total.textContent = result;
+// });
+// ამოცანა 9:
+// დაწერე ფუნქცია reverseString, რომელიც თავის ტანში შემოსულ სტრინგს დააბრუნებს
+// საპირისპირო მიმართულებით, გამოიყენეთ reverce()
+
+function reverseString(str){
+    return str.split('').reverse().join('');
+}
+const testCases= [
+    {input:"hello", expected: "olleh"},
+    {input:"world", expected: "dlrow"},
+    {input:"racecar", expected: "racecar"}
+
+];
+testCases.forEach(testCase=> {
+    const
 });
