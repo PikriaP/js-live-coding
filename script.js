8f0a2ace57d65c99645a983a86e814b0cba2581e
PikriaP <ffilishvili@gmail.com>
2024-11-30 02:41:11 +0400

    second version

--- script.js
+++ script.js
@@ -126,3 +126,20 @@
 //      }
 // }
 // checkPalindrome(word);
+// function isPalindrome (str){
+//     const length = str.length;
+//     for(let i=0; i<length /2; i++){
+//         if (str[i]!== str[length-i-1]){
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// const word = prompt('please enter a word: ');
+// if (isPalindrome(word)){
+//     console.log(`${word} is a palindrome!`);
+// }else{
+//     console.log(` ${word} is not a palindrome`);
+      
+// }
