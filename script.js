414757aebe104ce6871b4474b2dade54e73eed16
PikriaP <ffilishvili@gmail.com>
2024-11-30 00:08:48 +0400

    ex 9 and 10

--- script.js
+++ script.js
@@ -20,15 +20,51 @@
 // დაწერე ფუნქცია reverseString, რომელიც თავის ტანში შემოსულ სტრინგს დააბრუნებს
 // საპირისპირო მიმართულებით, გამოიყენეთ reverce()
 
-function reverseString(str){
-    return str.split('').reverse().join('');
-}
-const testCases= [
-    {input:"hello", expected: "olleh"},
-    {input:"world", expected: "dlrow"},
-    {input:"racecar", expected: "racecar"}
+// function reverseString(str){
+//     return str.split('').reverse().join('');
+// }
+// const testCases= [
+//     {input:"hello", expected: "olleh"},
+//     {input:"world", expected: "dlrow"},
+//     {input:"maria", expected: "dlrow"},
+//     {input:"racecar", expected: "racecar"}
 
-];
-testCases.forEach(testCase=> {
-    const
-});
+// ];
+// testCases.forEach(testCase=> {
+//     const result = reverseString(testCase.input);
+//     if(result === testCase.expected) {
+//         console.log(`test passed for input:  ${testCase.input}`);
+//     }else{
+//         console.error(`test failed for input : $ {testCase.input}. Expected: ${testCase.expected}, Got: ${result}`);
+        
+//     }
+        
+// });
+
+// ამოცანა 10:
+// დაწერე ფუნქცია checkVariable, რომელიც ამოწმებს არის თუ არა შემოსული ინფორმაცია,
+// უნდეფაინდი ან null და დაპრინტავს
+// The variable is undefined or null
+// The variable is neither undefined nor null
+// გადაეცით შემდეგი არგუმენტები :
+// let newVariable;
+// checkVariable(5);
+// checkVariable('hello');
+// checkVariable(null);
+// checkVariable(newVariable);
+
+
+// function checkVariable(variable){
+//     if (variable === undefined||variable===null){
+//         console.log("the variable is undefined or null");
+        
+//     }
+// }
+
+// let newVariable;
+// checkVariable(5);
+// checkVariable('hello');
+// checkVariable(null);
+// checkVariable(newVariable);
+
+
