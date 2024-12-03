e4a755926f134d09711181fb54373a0d3ed4fae6
PikriaP <ffilishvili@gmail.com>
2024-12-03 21:27:19 +0400

    exx  1

--- script.js
+++ script.js
@@ -244,4 +244,21 @@
 // let newVariable; 
 // checkVariable("");
 
-const value1Input = document.getElementById('value1');
+// const value1Input = document.getElementById('value1');
+// const staticValueElement = document.getElementById('value2');
+// const total = document.getElementById('total');
+
+// inputElement.addEventListener('input', () => {
+//     const inputValue = parseFloat(inputElement.value);
+//     const result = inputValue * parseInt(staticValueElement.textContent);
+
+//     if (result > 0) {
+//         total.classList.remove('other');
+//         total.classList.add('positive');
+//     } else {
+//         total.classList.remove('positive');
+//         total.classList.add('other');
+//     }
+
+//     total.textContent = result;
+// });
